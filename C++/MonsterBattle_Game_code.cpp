fcf6f248f6f469a150bf9f73f337844dffdd297f
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-04-08 18:59:46 +0530

    .

--- C++/MonsterBattle_Game_code.cpp
+++ C++/MonsterBattle_Game_code.cpp
@@ -49,12 +49,12 @@ class Monster
 		
 		bool Move1(Monster &obj)
 		{
+			hp -= moves[0][2];
 			
 			if(moves[0][3] >= rand()%100+1)
 			{
 				obj.hp -= moves[0][0];
 				hp += moves[0][1];
-				hp -= moves[0][2];
 				return true;
 			}
 			
@@ -67,12 +67,12 @@ class Monster
 		
 		bool Move2(Monster &obj)
 		{
+			hp -= moves[1][2];
 			
 			if(moves[1][3] >= rand()%100+1)
 			{
 				obj.hp -= moves[1][0];
 				hp += moves[1][1];
-				hp -= moves[1][2];
 				return true;
 			}
 			
@@ -85,12 +85,12 @@ class Monster
 		
 		bool Move3(Monster &obj)
 		{
+			hp -= moves[2][2];
 			
 			if(moves[2][3] >= rand()%100+1)
 			{
 				obj.hp -= moves[2][0];
 				hp += moves[2][1];
-				hp -= moves[2][2];
 				return true;
 			}
 			
@@ -104,31 +104,31 @@ class Monster
 
 //create as many monsters as you want!
 Monster Monsters[] = {
-	Monster("Pikachu",250,80,0,0,80,35,10,0,90,50,0,20,100),
-	Monster("Charizard",400,75,0,10,60,125,0,40,40,90,5,0,20),
-	Monster("Greninja",325,20,20,0,80,85,0,15,65,45,5,0,35),
-	Monster("Blaziken",375,100,0,50,25,0,50,0,40,55,5,0,50),
-	Monster("Bisharp",300,200,0,100,10,10,10,0,90,150,0,50,25),
-	Monster("Hoopa",500,1000,0,400,15,0,100,0,10,15,15,0,55),
-	Monster("Alakazam",275,25,25,0,95,190,0,100,65,65,0,25,70),
-	Monster("Gyarados", 450, 90, 0, 30, 70, 120, 0, 50, 50, 70, 0, 0, 25),
-	Monster("Dragonite", 500, 90, 10, 0, 40, 120, 0, 20, 20, 70, 0, 200, 100),
-	Monster("Mewtwo", 600, 100, 0, 50, 70, 130, 0, 50, 15, 20, 0, 0, 100),
-	Monster("Snorlax", 700, 80, 50, 0, 20, 0, 100, 0, 8, 250, 0, 500, 80),
-	Monster("Gengar", 350, 50, 0, 0, 90, 90, 0, 0, 25, 0, 50, 0, 30),
-	Monster("Tyranitar", 600, 120, 0, 20, 30, 100, 0, 50, 30, 80, 0, 0, 12),
-	Monster("Lucario", 400, 80, 20, 0, 10, 90, 0, 0, 25, 40, 0, 80, 40),
-	Monster("Salamence", 500, 100, 0, 500, 100, 120, 0, 0, 25, 70, 10, 0, 25),
-	Monster("Metagross", 550, 110, 0, 30, 50, 90, 0, 50, 25, 70, 0, 0, 30),
-	Monster("Haxorus", 550, 60, 0, 0, 60, 70, 0, 0, 10, 80, 0, 80, 20),
-	Monster("Hydreigon", 600, 110, 0, 0, 17, 120, 0, 20, 25, 70, 0, 0, 30),
-	Monster("Zoroark", 400, 40, 0, 0, 80, 70, 0, 0, 40, 100, 0, 0, 10),
-	Monster("Volcarona", 450, 0, 20, 0, 70, 110, 0, 30, 25, 80, 0, 0, 20),
-	Monster("Chandelure", 400, 90, 0, 0, 23, 110, 0, 20, 25, 70, 0, 0, 30),
-	Monster("Galvantula", 350, 70, 0, 250, 90, 60, 0, 0, 10, 60, 0, 0, 35),
-	Monster("Krookodile", 450, 100, 0, 20, 30, 70, 0, 0, 35, 80, 0, 0, 25),
-	Monster("Excadrill", 500, 100, 0, 20, 50, 120, 0, 120, 25, 80, 0, 0, 20),
-	Monster("Froslass", 350, 30, 0, 0, 80, 80, 0, 0, 30, 60, 0, 0, 35)
+	Monster("Pikachu",250,80,0,20,80,35,10,0,90,50,0,20,100),
+	Monster("Charizard",400,75,0,30,60,125,0,50,40,90,5,0,20),
+	Monster("Greninja",325,20,20,0,80,85,0,35,65,45,5,10,35),
+	Monster("Blaziken",375,100,0,50,25,0,50,0,40,55,5,15,50),
+	Monster("Bisharp",300,200,0,100,40,10,10,0,90,150,0,50,35),
+	Monster("Hoopa",500,1000,0,400,45,0,100,0,20,25,15,0,55),
+	Monster("Alakazam",275,25,25,0,95,190,0,100,75,65,0,15,70),
+	Monster("Gyarados", 450, 90, 0, 30, 70, 120, 0, 50, 50, 70, 0, 0, 15),
+	Monster("Dragonite", 500, 90, 10, 20, 40, 120, 0, 60, 60, 115, 0, 200, 100),
+	Monster("Mewtwo", 600, 100, 0, 50, 70, 130, 0, 50, 45, 20, 0, 5, 100),
+	Monster("Snorlax", 700, 80, 50, 0, 17, 0, 100, 5, 15, 250, 0, 300, 80),
+	Monster("Gengar", 350, 40, 0, 0, 90, 90, 0, 30, 65, 0, 50, 0, 30),
+	Monster("Tyranitar", 600, 120, 0, 50, 50, 100, 0, 50, 30, 80, 0, 20, 72),
+	Monster("Lucario", 400, 80, 20, 0, 10, 90, 0, 5, 25, 70, 0, 80, 40),
+	Monster("Salamence", 500, 100, 0, 200, 100, 120, 0, 13, 25, 70, 10, 30, 65),
+	Monster("Metagross", 550, 110, 0, 60, 50, 90, 0, 50, 55, 70, 0, 80, 90),
+	Monster("Haxorus", 550, 60, 0, 20, 60, 70, 0, 50, 50, 80, 0, 80, 60),
+	Monster("Hydreigon", 600, 110, 0, 2, 10, 120, 0, 25, 35, 70, 0, 20, 60),
+	Monster("Zoroark", 400, 40, 0, 20, 80, 70, 0, 10, 40, 100, 0, 1, 10),
+	Monster("Volcarona", 450, 0, 20, 3, 70, 110, 0, 80, 75, 80, 0, 15, 35),
+	Monster("Chandelure", 400, 40, 0, 0, 25, 110, 0, 40, 55, 70, 0, 10, 30),
+	Monster("Galvantula", 350, 70, 0, 100, 90, 60, 0, 0, 10, 60, 0, 20, 45),
+	Monster("Krookodile", 450, 100, 0, 20, 30, 70, 0, 70, 85, 80, 0, 20, 55),
+	Monster("Excadrill", 500, 100, 0, 50, 50, 120, 0, 120, 75, 80, 0, 0, 10),
+	Monster("Froslass", 350, 20, 0, 0, 80, 80, 0, 5, 40, 60, 0, 0, 25)
 };//All the monsters should be inside this string data type array
 
 void game()
@@ -136,15 +136,22 @@ void game()
 	//Attacking other monster -- Monster1.Move1/Move2/Move3(Monster2);
 	bool undefeated = true;
 	char printMonstersAgain = 'y';
-	int MatchesWon = 0,user_monster, computer_monster, user_move, computer_move;
+	int MatchesWon = 0, MatchesPlayed = 0, user_monster, computer_monster, user_move, computer_move;
 	
 	//Game starts
-	cout<<"--------------------\nMONSTER BATTLE !\n-------------------"<<endl;
-	this_thread::sleep_for(chrono::seconds(1));
+	cout<<"--------------------\nMONSTER BATTLE !\n-------------------\n\n-------------------\nNOTE : \n\t1) Game will end if you lose a battle.\n\t2) You score +1 for every win and -1 for every draw.\n\t4) Every monster has 3 moves.\n\t3) Monsters's HP will get self-damaged even if the move misses\n\t4) Computers's monster will be different from yours.\n-------------------\n"<<endl;
 	
 	while(undefeated)
 	{
-		cout<<"\n--------------------\nMatch "<<MatchesWon+1<<"\n--------------------\n"<<endl;
+		cout<<"\n-------------------\nMatch "<<MatchesPlayed+1<<" starts in : "<<endl;
+		//Countdown
+		for(int i = 10;i > 0;i--)
+		{
+			cout<<"\t"<<i<<endl;
+			this_thread::sleep_for(chrono::seconds(1));
+		}
+		
+		cout<<"\n--------------------\nMatch "<<MatchesPlayed+1<<"\n--------------------\n"<<endl;
 		this_thread::sleep_for(chrono::seconds(2));
 		
 		if(printMonstersAgain == 'y')
@@ -329,6 +336,8 @@ void game()
 				if(Monsters[computer_monster].getHP() <= 0 && Monsters[user_monster-1].getHP() <= 0)
 				{
 					cout<<"\n--------------------\nDRAW !\n--------------------"<<endl;
+					MatchesWon -= 1;
+					MatchesPlayed += 1;
 					this_thread::sleep_for(chrono::seconds(1));
 					Monsters[computer_monster].setHPtoMax();
 					Monsters[user_monster-1].setHPtoMax();
@@ -340,6 +349,7 @@ void game()
 					cout<<"\n--------------------\nYOU WIN !\n--------------------"<<endl;
 					this_thread::sleep_for(chrono::seconds(1));
 					MatchesWon += 1;
+					MatchesPlayed += 1;
 					Monsters[computer_monster].setHPtoMax();
 					Monsters[user_monster-1].setHPtoMax();
 					cout<<"\n--------------------\nDo want to get the details of all the monsters for your next match? (y/n) : ";
