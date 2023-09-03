4008e31901156157d0da30a660ffffff5dcf5aa7
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-09-03 14:46:28 +0530

    Create `factorial.cs`

---
+++ C#/factorial.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Code
+{
+    public class Program
+    {
+        public static int factorial(int num)
+        {
+            int val = 1;
+            
+            for(;num > 1;num = num - 1)
+            {
+                val = val * num;
+            }
+            
+            return val;
+        }
+        
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The factorial of {num} is {factorial(num)}");
+        }
+    }
+}
