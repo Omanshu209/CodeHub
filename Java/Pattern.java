74d78a76eae99facb51afc628d57d17b40b05728
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-09-29 18:17:14 +0530

    Create `Pattern.java`

---
+++ Java/Pattern.java
@@ -0,0 +1,26 @@
+/*
+5 , 
+10 , 4 , 
+15 , 8 , 3 , 
+20 , 12 , 6 , 2 , 
+25 , 16 , 9 , 4 , 1 , 
+*/
+
+public class pattern
+{
+	public static void main(String[] args)
+	{
+		for(int i = 1 ; i <= 5 ; i++)
+		{
+			int f = i;
+			
+			for(int j = 1 ; j <= i ; j++)
+			{
+				System.out.print((f * (6 - j)) + " , ");
+				f--;
+			}
+			
+			System.out.println();
+		}
+	}
+}
