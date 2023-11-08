ed025b523b40e600bbdca11389d6a6c2552645f7
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-11-08 19:19:52 +0530

    Update `PATTERN_HourGlass_2.java`

--- Java/PATTERN_HourGlass_2.java
+++ Java/PATTERN_HourGlass_2.java
@@ -1,22 +1,19 @@
-/*
-*******
- *   *
-  * *
-   *
-  * *
- *   *
-*******
-*/
+import java.util.Scanner;
 
 public class Hour_Glass_PATTERN_2
 {
 	public static void main(String[] args)
 	{
-		for(int i = 1 ; i <= 7 ; i++)
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("n : ");
+		int n = sc.nextInt();
+		
+		for(int i = 1 ; i <= n ; i ++)
 		{
-			for(int j = 1 ; j <= 7 ; j++)
+			for(int j = 1 ; j <= n ; j++)
 			{
-				if((i % 7 <= 1) || (i == j) || (i + j == 8))
+				if((j % n <= 1) || (i == j) || (i + j == n + 1))
 					System.out.print("*");
 				
 				else
