00a6d2ec784b9303f5ec893d983dcfb4b7886b20
Omanshu <114089324+Omanshu209@users.noreply.github.com>
2023-11-10 15:29:47 +0530

    Update `CoOrdinate_Geometry_Graph.java`

--- Java/CoOrdinate_Geometry_Graph.java
+++ Java/CoOrdinate_Geometry_Graph.java
@@ -1,10 +1,16 @@
 /*
 Example 1 : 
 
+Input :
+
 x = 5
 y = -2
 x intercept = 4
-Show Co-ordinates (true / false) : false
+Show Co-ordinates (y/n) : n
+Maximum Co-ordinate : 10
+
+Output:
+
 y_intercept = 8.0
 Slope = -2.0
                   * |
@@ -33,10 +39,16 @@ Slope = -2.0
 
 Example 2 :
 
+Input :
+
 x = 3
 y = -3
 x intercept = 6
-Show Co-ordinates (true / false) : true
+Show Co-ordinates (y/n) : y
+Maximum Co-ordinate : 10
+
+Output:
+
 y_intercept = -6.0
 Slope = 1.0
                     |
@@ -65,42 +77,51 @@ Slope = 1.0
 
 Example 3 :
 
+Input :
 x = -3
 y = 3
 x intercept = 6
-Show Co-ordinates (true / false) : true
+Show Co-ordinates (y/n) : y
+Maximum Co-ordinate : 12
+
+Output :
+
 y_intercept = 2.0
 Slope = -0.3333333333333333
 [NOTE : The line below isn't straight since 1 or more of the values above is not an integer]
 [NOTE : "Show Co-Ordinate" feature is not available for these inputs as 1 or more of the values above is not an integer]
-                    |
-                    |
-                    |
-                    |
-                    |
-  *                 |
-        *           |
-              *     |
-                    *
-                    |     *
---------------------------------*--------
-                    |                 *
-                    |
-                    |
-                    |
-                    |
-                    |
-                    |
-                    |
-                    |
-                    |
+                        |
+                        |
+                        |
+                        |
+                        |
+                        |
+*                       |
+      *                 |
+            *           |
+                  *     |
+                        *
+                        |     *
+------------------------------------*------------
+                        |                 *
+                        |                       *
+                        |
+                        |
+                        |
+                        |
+                        |
+                        |
+                        |
+                        |
+                        |
+                        |
 */
 
 import java.util.Scanner;
 
 public class CoOrdinate_Geometry
 {
-	static void showLine(double x, double y, double x_intercept, boolean showCoordinates)
+	static void showLine(double x, double y, double x_intercept, boolean showCoordinates, int maxUnit)
 	{
 		try
 		{
@@ -117,29 +138,47 @@ public class CoOrdinate_Geometry
 			if(!(y_intercept % 1 == 0 && slope % 1 == 0))
 			{
 				isDecimal = true;
-				System.out.println("[NOTE : The line below isn't straight since 1 or more of the values above is not an integer]");
+				
+				System.out.println(
+				"\u001B[31m" + 
+				"[NOTE : The line below isn't straight since 1 or more of the values above is not an integer]" + 
+				"\u001B[0m"
+				);
 				
 				if(showCoordinates)
 				{
 					showCoordinates = false;
-					System.out.println("[NOTE : \"Show Co-Ordinate\" feature is not available for these inputs as 1 or more of the values above is not an integer]");
+					
+					System.out.println(
+					"\u001B[31m" + 
+					"[NOTE : \"Show Co-Ordinate\" feature is not available for these inputs as 1 or more of the values above is not an integer]" + 
+					"\u001B[0m"
+					);
 				}
 			}
 			
-			for(int i = 10 ; i >= -10 ; i--)
+			for(int i = maxUnit ; i >= -maxUnit ; i--)
 			{
 				// Straight Line Equation : y = mx + c
 				double new_x = (i - y_intercept) / slope;
 							
-				for(double j = -10 ; j <= 10 ; j += 0.5)
+				for(double j = -maxUnit ; j <= maxUnit ; j += 0.5)
 				{
 					if(j == new_x || (j == (int)new_x && isDecimal))
 					{
 						if(showCoordinates)
-							System.out.print("*  " + "(" + j + "," + i + ")");
+							System.out.print(
+							"\u001B[32m" + 
+							"*  " + 
+							"\u001B[0m" + 
+							"(" + j + "," + i + ")"
+							);
 						
 						else
-							System.out.print('*');
+							System.out.print(
+							"\u001B[32m" + 
+							"*" + 
+							"\u001B[0m");
 					}
 					
 					else
@@ -149,7 +188,11 @@ public class CoOrdinate_Geometry
 							if(showCoordinates && (new_x + 5 > j) && (new_x < j))
 								continue;
 							
-							char printVal = (i == 0) ? '-' : '|';
+							String printVal = 
+							(i == 0)
+							? "\u001B[34m" + "-" + "\u001B[0m" 
+							: "\u001B[34m" + "|" + "\u001B[0m";
+							
 							System.out.print(printVal);
 						}
 						
@@ -184,10 +227,16 @@ public class CoOrdinate_Geometry
 		System.out.print("x intercept = ");
 		double x_intercept = sc.nextDouble();
 		
-		System.out.print("Show Co-ordinates (true / false) : ");
-		boolean showCoordinates = sc.nextBoolean();
+		System.out.print("Show Co-ordinates (y/n) : ");
+		boolean showCoordinates = 
+		(sc.next().charAt(0) == 'y') 
+		? true 
+		: false;
 		
-		showLine(x, y, x_intercept, showCoordinates);
+		System.out.print("Maximum Co-ordinate : ");
+		int maxUnit = sc.nextInt();
+		
+		showLine(x, y, x_intercept, showCoordinates, maxUnit);
 	}
 }
 //Developed by Omanshu
